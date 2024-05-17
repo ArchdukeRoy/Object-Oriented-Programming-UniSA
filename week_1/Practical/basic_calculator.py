a592d4e01840565ffffbd21f98ccee9c70521793
Roy Vallecera <royvallecera.01@gmail.com>
2024-05-17 22:59:17 +0930

    basic_calculator: square_number function completed

---
+++ week_1/Practical/basic_calculator.py
@@ -0,0 +1,27 @@
+"""
+Task 4: Revisiting Python Programming
+Whether you are a beginner programmer or have been coding for years; you should always
+endeavour to improve your technical and problem-solving skills. It is time for you, your
+programming partner, and your rubber ducky to get programming!
+
+Problem Statement
+Functions are tools for software developers which can be implemented into any software.
+Create three functions with different uses.
+1. Write a function to square a number.
+2. Write a function which checks if any given number is odd or even.
+3. Write a function which calculates the average in a list of numbers.
+
+"""
+
+
+def square_number(num):
+    squared = num * num
+    return num
+
+
+def odd_even(num):
+    pass
+
+
+def average(num):
+    pass
