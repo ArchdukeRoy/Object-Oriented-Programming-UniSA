6d7f24b6712e0fde9bcda43a73c37d3af4fc9de7
Roy Vallecera <royvallecera.01@gmail.com>
2024-05-18 08:22:23 +0930

    pokemon.txt and pokemon_hangman.py files added

---
+++ week_1/Workshop/pokemon_hangman.py
@@ -0,0 +1,27 @@
+"""
+Problem Statement
+You and your partner will need to make a “guess the word” style game. This game should take
+hidden words from a text file which has been provided for you (Resource 1). These words can
+later be replaced to a theme of your choice.
+The game is simple. Upon starting, a random word should be chosen from the file. You then
+take turns guessing until the word is revealed. You have unlimited turns and should keep track
+of your turns. Once the game has ended, the word is revealed and how many turns it took to
+guess it.
+This type of exercise will test your knowledge of opening and closing files, random, lists,
+strings, and while loops. You can implement this in any way you like but it should be authentic
+to you and your partners level of knowledge. You do not need to implement this with classes
+or objects (It is only week 1 for goodness sake). Each function and its responsibilities are listed
+below.
+"""
+
+
+def choose_ward(file_path):
+    pass
+
+
+def display_word():
+    pass
+
+
+def play_game():
+    pass
